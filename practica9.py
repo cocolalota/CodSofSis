50fdaf9be29fe148158828f03994bccea2242c9c
cocolalota <palacios.victor.2024@cbtis-246.edu.mx>
2025-02-27 14:46:16 -0600

    Add files via upload

--- practica9.py
+++ practica9.py
@@ -1,66 +1,40 @@
+#victor jovany palacios noguez
+#2-J
+#practica 9
+
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[15]:
 
 
-edad = float(input("ingresa la edad que deseas saber si es posible votar"))
-if edad >= 18:
-    print("es posible votar")
-else:
-    print("eres menor de edad, no puedes votar")
+for i in range (10):
+    print(i)
+for i in range (1,20,2):
+    print(i)
+
+lista1=[1,2,3,4,5,6,7,8,9,0]
+lista1.sort(reverse=true)
+print(lista1)
 
 
-# In[5]:
+# In[9]:
 
 
-numero1=input("ingresa el primer numero")
-numero2=input("ingresa el segundo numero")
-if numero1>numero2:
-    print(numero1," es mayor que ",numero2)
-elif numero2>numero1:
-    print(numero2," es mayor que ",numero1)
+numero=int(input("ingresa el numero que deseas multiplicar"))
+for i in range (1,11): 
+    print(i,"x",numero,"=",i*numero)
+    
 
 
-# In[11]:
+# In[13]:
 
 
-edades=[]
-edad1=int(input("ingresa tu edad"))
-edades.append(edad1)
-edad2=int(input("ingresa tu edad"))
-edades.append(edad2)
-edad3=int(input("ingresa tu edad"))
-edades.append(edad3)
-edad4=int(input("ingresa tu edad"))
-edades.append(edad4)
-print("la edad menor es ",min(edades))
-
-
-# In[21]:
-
-
-minus="qwertyuiopasdfghjklñzxcvbnm"
-mayus="QWERTYUIOPASDFGHJKLÑZXCVBNM"
-palabra=input("ingresa una palabra")
-letras=list(palabra)
-for letra in letras:
-    if letra in minus:
-        print("letra minuscula ",letra)
-    elif letra in mayus:
-        print("letra mayuscula ",letra)
-    else:
-        print("es un simbolo o numero ",letra)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+numero1="1234567890"
+contador=1
+numero=input("ingresa el numero")
+numero2=list(numero)
+for numero3 in numero2:
+    contador= int (numero3)*contador
+    print("producto",contador)
 
