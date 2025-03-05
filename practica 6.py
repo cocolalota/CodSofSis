69e3079a3d440cb6fabd3d22a8940ef9eeb61950
cocolalota <palacios.victor.2024@cbtis-246.edu.mx>
2025-03-05 15:38:46 -0600

    Add files via upload

---
+++ practica 6.py
@@ -0,0 +1,70 @@
+#nombre:victor jovany palacios noguez
+#fecha:20\02\25
+#grupo y grado:2-J
+#nombre: tupla
+
+
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+tupla_1=(1,2,3,4,5)
+print(tupla_1)
+
+
+# In[ ]:
+
+
+tupla_2=(27,7,8,9,10)
+print(tupla_2[2])
+
+
+# In[ ]:
+
+
+tupla_1(1,2,3,4,5)
+tupla_2(6,7,8,9,10)
+tupla_nueva=tupla_1+tupla_2
+print(tupla_nueva)
+
+
+# In[ ]:
+
+
+tupla=('a','b','c')
+print(tupla[0],tupla[1],tupla[2])
+
+
+# In[ ]:
+
+
+tupla=(1,2,3,4,50)
+print(2 in tupla
+
+
+# In[ ]:
+
+
+tupla=(0,1,2,3,4,5)
+print(tupla[2:4])
+
+
+# In[ ]:
+
+
+tupla=(1,2,3,4,5)
+tupla_repetida=tupla*3
+print(tupla_repetida)
+
+
+# In[ ]:
+
+
+tupla=(5,6,7,8,15)
+print(min(tupla))
+print(max(tupla))
+
